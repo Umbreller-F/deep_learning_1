45ba7af62f53948750415622c90c168ec41f7176
Umbreller-F <umbreller_f@163.com>
2022-04-05 09:57:39 +0800

    完成resnet

--- ResNet.py
+++ ResNet.py
@@ -28,6 +28,29 @@ class ResNetLayer(nn.Module):
         ########################    END   ##########################
 
 
+def ResNetBlock(in_feature_maps, out_feature_maps, layers):
+    blk = []
+    blk.append(ResNetLayer(in_feature_maps, out_feature_maps))
+    for _ in range(layers-1):
+        blk.append(ResNetLayer(out_feature_maps, out_feature_maps, downsample=False))
+    return nn.Sequential(*blk)
+
+
+def my_flatten():
+    blk = []
+    blk.append(nn.Flatten())
+    return nn.Sequential(*blk)
+
+
+def my_fc(in_features, out_features):
+    blk = []
+    blk.append(nn.Linear(in_features,out_features))
+    blk.append(nn.BatchNorm1d(out_features))
+    blk.append(nn.ReLU())
+    blk.append(nn.Dropout(0.15))
+    return nn.Sequential(*blk)
+
+
 class ResNet(nn.Module):
     def __init__(self):
         super(ResNet, self).__init__()
@@ -36,8 +59,16 @@ class ResNet(nn.Module):
         self.bn0 = nn.BatchNorm2d(64)
         
         ######################## task 2.2 ##########################
-
-
+        self.blk1 = ResNetBlock(64, 128, 3)
+        self.blk2 = ResNetBlock(128, 256, 4)
+        self.blk3 = ResNetBlock(256, 512, 6)
+        self.avgpool = nn.AvgPool2d(2, 2)
+        self.blk4 = ResNetLayer(512, 512, downsample=False)
+        self.flat = my_flatten()
+        self.fc1 = my_fc(4*4*512, 4*512)
+        # self.fc2 = nn.Linear(4*512, 512)
+        self.fc2 = my_fc(4*512, 512)
+        self.fc3 = nn.Linear(512, 200)
         ########################    END   ##########################
 
         self.dropout = nn.Dropout(0.15)
@@ -46,8 +77,15 @@ class ResNet(nn.Module):
         x = F.relu(self.bn0(self.dropout(self.conv0(input))))
         
         ######################## task 2.3 ##########################
-
-
+        x = self.blk1(x)
+        x = self.blk2(x)
+        x = self.blk3(x)
+        x = self.avgpool(x)
+        x = self.blk4(x)
+        x = self.flat(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
         ########################    END   ##########################
 
         return x
